9bf1e7e100e6c298442229b014f67850ebf15d93
Patrick Marschke <patrick.m.marschke@gmail.com>
2022-12-03 13:17:12 -0600

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Form from './Form.svelte';
+</script>
+
+<svelte:head>
+	<title>❏❐computermusic.club❑❒</title>
+</svelte:head>
+
+<main>
+	<h1>computermusic.club</h1>
+	<div class="sub">
+		<Form />
+	</div>
+</main>
+
+<style>
+	main {
+		font-family: monospace;
+	}
+</style>
