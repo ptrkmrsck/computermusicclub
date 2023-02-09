fd02f640edf4371a9609900147310a2be9147587
ptrk <patrick.m.marschke@gmail.com>
2023-02-09 16:50:04 -0600

    add event info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,11 +8,59 @@
 
 <main>
 	<h1>computermusic.club</h1>
-	<h3><em>❏❐ coming soon ❑❒</em></h3>
+	<p>
+		The times that I am most motivated to things is after talking to people that are excited and
+		curious about making things. It feels like connecting with people really seamlessly flows into
+		connecting ideas, and what is making a thing if not the simple act of connecting ideas?
+	</p>
+
+	<p>
+		Selfishly, I want more of this for myself, and to have some regularity around it, along with a
+		little splash of accountability. Non-selfishly, I have heard from a few folks in the Twin Cities
+		that they feel isolated in their computer-music-related creative pursuits. There is so much
+		knowledge and experience here, but there isn’t necessarily an easy way to exchange this
+		knowledge.
+	</p>
+
+	<p>
+		These are <em>some</em> of the reasons why I want computermusic.club to exist (there are MANY more!),
+		but I also think it is important to get as many perspectives on what would most benefit this “community”
+		as possible. For that reason, I imagine this first meeting being more of an open forum. Anyone that
+		has ideas or questions about the concept will have a chance to be heard. From there we will decide
+		which things to try to make happen first, and then figure out how to make those things happen. Very
+		exciting!
+	</p>
+
+	<p>
+		I am planning on recording and/or taking notes on the discussion, and generally being really
+		“open source” about the whole thing. Folks can contribute in whatever way they have the capacity
+		to.
+	</p>
+
+	<p>Let’s learn and make stuff with computers!</p>
+	<p><a href="https://noplace.place">-ptrk</a></p>
+
+	<img src="/feb-smol.jpg" alt="" />
+	<h2>
+		computermusic.club inaugural meeting <br />
+		Saturday, February 11, 4:30-6:30pm <br />
+		at RESOURCE // 512 24th St E, Minneapolis, MN 55404
+	</h2>
+
+	<p>All are welcome! Bring a friend!</p>
+	<p><a href="https://www.facebook.com/events/904960880696849">fb event</a></p>
+
+	<h3><em>❏❐ more stuff coming soon :) ❑❒</em></h3>
 </main>
 
 <style>
 	main {
 		font-family: monospace;
+	}
+	img {
+		width: 50vw;
+	}
+	a:visited {
+		color: blue;
 	}
 </style>
